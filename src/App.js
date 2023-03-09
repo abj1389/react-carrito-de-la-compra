11b358d23e20bcff2828101920c24160fcf972e3
abj1389 <abj_13@hotmail.com>
2023-03-09 15:18:35 +0100

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import TodoList from './TodoList/TodoList';
 import ShoppingCart from './ShoppingCart/ShoppingCart';
 
 function App() {
   return (
     <div className="App">
-      <h2>Lista de tareas:</h2>
-      <TodoList></TodoList>
       <h2>Carrito de la compra:</h2>
       <ShoppingCart></ShoppingCart>
     </div>
